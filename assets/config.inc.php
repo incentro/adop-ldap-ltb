9273767d3b03152a5b202ea33a8f1aeaeb440ec4
aevitas <m4rtijn@gmail.com>
2017-10-23 14:09:35 +0200

    Update password manager version to 1.1

--- assets/config.inc.php
+++ assets/config.inc.php
@@ -22,13 +22,18 @@
 #==============================================================================
 # Configuration
 #==============================================================================
+
+# Debug mode
+# true: log and display any errors or warnings (use this in configuration/testing)
+# false: log only errors and do not display them (use this in production)
+$debug = false;
+
 # LDAP
-
-$ldap_url = getenv('LDAP_LTB_URL');
-$ldap_binddn = getenv('LDAP_LTB_DN');
-$ldap_bindpw = getenv('LDAP_LTB_PWD');
-$ldap_base = getenv('LDAP_LTB_BS');
-
+$ldap_url = "ldap://localhost";
+$ldap_starttls = false;
+$ldap_binddn = "cn=manager,dc=example,dc=com";
+$ldap_bindpw = "secret";
+$ldap_base = "dc=example,dc=com";
 $ldap_login_attribute = "uid";
 $ldap_fullname_attribute = "cn";
 $ldap_filter = "(&(objectClass=person)($ldap_login_attribute={login}))";
@@ -41,53 +46,69 @@ $ad_mode = false;
 $ad_options['force_unlock'] = false;
 # Force user change password at next login
 $ad_options['force_pwd_change'] = false;
+# Allow user with expired password to change password
+$ad_options['change_expired_password'] = false;
 
 # Samba mode
 # true: update sambaNTpassword and sambaPwdLastSet attributes too
 # false: just update the password
 $samba_mode = false;
+# Set password min/max age in Samba attributes
+#$samba_options['min_age'] = 5;
+#$samba_options['max_age'] = 45;
 
 # Shadow options - require shadowAccount objectClass
 # Update shadowLastChange
 $shadow_options['update_shadowLastChange'] = false;
+$shadow_options['update_shadowExpire'] = false;
+
+# Default to -1, never expire
+$shadow_options['shadow_expire_days'] = -1;
 
 # Hash mechanism for password:
-# SSHA
-# SHA
+# SSHA, SSHA256, SSHA384, SSHA512
+# SHA, SHA256, SHA384, SHA512
 # SMD5
 # MD5
 # CRYPT
 # clear (the default)
+# auto (will check the hash of current password)
 # This option is not used with ad_mode = true
-$hash = "SHA";
+$hash = "SHA256";
+
+# Prefix to use for salt with CRYPT
+$hash_options['crypt_salt_prefix'] = "$6$";
+$hash_options['crypt_salt_length'] = "6";
 
 # Local password policy
 # This is applied before directory password policy
 # Minimal length
-$pwd_min_length = 9;
+$pwd_min_length = 0;
 # Maximal length
 $pwd_max_length = 0;
 # Minimal lower characters
-$pwd_min_lower = 6;
+$pwd_min_lower = 0;
 # Minimal upper characters
-$pwd_min_upper = 1;
+$pwd_min_upper = 0;
 # Minimal digit characters
-$pwd_min_digit = 1;
+$pwd_min_digit = 0;
 # Minimal special characters
-$pwd_min_special = 1;
+$pwd_min_special = 0;
 # Definition of special characters
 $pwd_special_chars = "^a-zA-Z0-9";
 # Forbidden characters
 #$pwd_forbidden_chars = "@%";
 # Don't reuse the same password as currently
 $pwd_no_reuse = true;
+# Check that password is different than login
+$pwd_diff_login = true;
 # Complexity: number of different class of character required
 $pwd_complexity = 0;
 # Show policy constraints message:
 # always
 # never
 # onerror
-$pwd_show_policy = "onerror";
+$pwd_show_policy = "never";
 # Position of password policy constraints message:
 # above - the form
 # below - the form
@@ -99,11 +120,32 @@ $pwd_show_policy_pos = "above";
 # manager: the above binddn
 $who_change_password = "user";
 
+## Standard change
+# Use standard change form?
+$use_change = true;
+
+## SSH Key Change
+# Allow changing of sshPublicKey?
+$change_sshkey = false;
+
+# What attribute should be changed by the changesshkey action?
+$change_sshkey_attribute = "sshPublicKey";
+
+# Who changes the sshPublicKey attribute?
+# Also applicable for question/answer save
+# user: the user itself
+# manager: the above binddn
+$who_change_sshkey = "user";
+
+# Notify users anytime their sshPublicKey is changed
+## Requires mail configuration below
+$notify_on_sshkey_change = false;
+
 ## Questions/answers
 # Use questions/answers?
 # true (default)
 # false
-$use_questions = false;
+$use_questions = true;
 
 # Answer attribute should be hidden to users!
 $answer_objectClass = "extensibleObject";
@@ -116,7 +158,7 @@ $answer_attribute = "info";
 # Use tokens?
 # true (default)
 # false
-$use_tokens = false;
+$use_tokens = true;
 # Crypt tokens?
 # true (default)
 # false
@@ -127,40 +169,87 @@ $token_lifetime = "3600";
 ## Mail
 # LDAP mail attribute
 $mail_attribute = "mail";
+# Get mail address directly from LDAP (only first mail entry)
+# and hide mail input field
+# default = false
+$mail_address_use_ldap = false;
 # Who the email should come from
 $mail_from = "admin@example.com";
+$mail_from_name = "Self Service Password";
+$mail_signature = "";
 # Notify users anytime their password is changed
 $notify_on_change = false;
+# PHPMailer configuration (see https://github.com/PHPMailer/PHPMailer)
+$mail_sendmailpath = '/usr/sbin/sendmail';
+$mail_protocol = 'smtp';
+$mail_smtp_debug = 0;
+$mail_debug_format = 'html';
+$mail_smtp_host = 'localhost';
+$mail_smtp_auth = false;
+$mail_smtp_user = '';
+$mail_smtp_pass = '';
+$mail_smtp_port = 25;
+$mail_smtp_timeout = 30;
+$mail_smtp_keepalive = false;
+$mail_smtp_secure = 'tls';
+$mail_contenttype = 'text/plain';
+$mail_wordwrap = 0;
+$mail_charset = 'utf-8';
+$mail_priority = 3;
+$mail_newline = PHP_EOL;
 
 ## SMS
 # Use sms
-$use_sms = false;
+$use_sms = true;
+# SMS method (mail, api)
+$sms_method = "mail";
+$sms_api_lib = "lib/smsapi.inc.php";
 # GSM number attribute
 $sms_attribute = "mobile";
+# Partially hide number
+$sms_partially_hide_number = true;
 # Send SMS mail to address
 $smsmailto = "{sms_attribute}@service.provider.com";
 # Subject when sending email to SMTP to SMS provider
 $smsmail_subject = "Provider code";
 # Message
 $sms_message = "{smsresetmessage} {smstoken}";
-
+# Remove non digit characters from GSM number
+$sms_sanitize_number = false;
+# Truncate GSM number
+$sms_truncate_number = false;
+$sms_truncate_number_length = 10;
 # SMS token length
 $sms_token_length = 6;
+# Max attempts allowed for SMS token
+$max_attempts = 3;
+
+# Encryption, decryption keyphrase, required if $crypt_tokens = true
+# Please change it to anything long, random and complicated, you do not have to remember it
+# Changing it will also invalidate all previous tokens and SMS codes
+$keyphrase = "secret";
+
+# Reset URL (if behind a reverse proxy)
+#$reset_url = $_SERVER['HTTP_X_FORWARDED_PROTO'] . "://" . $_SERVER['HTTP_X_FORWARDED_HOST'] . $_SERVER['SCRIPT_NAME'];
 
 # Display help messages
 $show_help = true;
 
-# Language
-$lang ="en";
+# Default language
+$lang = "en";
+
+# List of authorized languages. If empty, all language are allowed.
+# If not empty and the user's browser language setting is not in that list, language from $lang will be used.
+$allowed_lang = array();
+
+# Display menu on top
+$show_menu = true;
 
 # Logo
-$logo = "style/ltb-logo.png";
+$logo = "images/ltb-logo.png";
 
-# Debug mode
-$debug = false;
-
-# Encryption, decryption keyphrase
-$keyphrase = "secret";
+# Background image
+$background_image = "images/unsplash-space.jpeg";
 
 # Where to log password resets - Make sure apache has write permission
 # By default, they are logged in Apache log
@@ -173,15 +262,17 @@ $login_forbidden_chars = "*()&|";
 
 ## CAPTCHA
 # Use Google reCAPTCHA (http://www.google.com/recaptcha)
-# Go on the site to get public and private key
 $use_recaptcha = false;
+# Go on the site to get public and private key
 $recaptcha_publickey = "";
 $recaptcha_privatekey = "";
-# Customize theme (see http://code.google.com/intl/de-DE/apis/recaptcha/docs/customization.html)
-# Examples: red, white, blackglass, clean
-$recaptcha_theme = "white";
-# Force HTTPS for recaptcha HTML code
-$recaptcha_ssl = false;
+# Customization (see https://developers.google.com/recaptcha/docs/display)
+$recaptcha_theme = "light";
+$recaptcha_type = "image";
+$recaptcha_size = "normal";
+# reCAPTCHA request method, null for default, Fully Qualified Class Name to override
+# Useful when allow_url_fopen=0 ex. $recaptcha_request_method = '\ReCaptcha\RequestMethod\CurlPost';
+$recaptcha_request_method = null;
 
 ## Default action
 # change
@@ -194,4 +285,5 @@ $default_action = "change";
 #$messages['passwordchangedextramessage'] = NULL;
 #$messages['changehelpextramessage'] = NULL;
 
-?>
+# Launch a posthook script after successful password change
+#$posthook = "/usr/share/self-service-password/posthook.sh";
